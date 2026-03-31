a13ef95a56c1b23b06c60c95005fa899f4f8c1fe
Ana Carolina <kalluhxl@gmail.com>
2026-03-31 09:05:22 -0300

    meme to nervoso

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿ class Program
+{
+    static void Main()
+    {
+        Console.Write("Usuário, o que você pensa sobre mim? ");
+        string texto = Console.ReadLine();
+
+        Console.WriteLine("\n- E sabe o que eu penso de você?");
+        Console.WriteLine(texto.Substring(0, Math.Min(10, texto.Length)) + "... NÃO, PERA.");
+        Console.WriteLine("Tô nervoso");
+    }
+}
